9ba1f0d9683c2001bd525fa834bac95add0c1935
Andrew Downing <andrew.c.downing@gmail.com>
2023-01-03 21:12:10 -0700

    initial commit

---
+++ noxfile.py
@@ -0,0 +1,7 @@
+# noxfile.py
+import nox
+@nox.session(python=["3.10"])
+def tests(session):
+    args = session.posargs or ["--cov"]
+    session.run("poetry", "install", external=True)
+    session.run("pytest", *args)
